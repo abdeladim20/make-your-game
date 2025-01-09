3266e2cfadc74de460bf5738369c6636d0eb20d3
ICEBergJQ <salahsk52@gmail.com>
2025-01-09 22:31:17 +0100

    file structure initialized

--- Enemy.js
+++
@@ -1,9 +0,0 @@
-export default class Enemy {
-  constructor(x, y, imageNumber) {
-    this.x = x;
-    this.y = y;
-
-    this.image = new image();
-    this.image.src = `images/enemy${imageNumber}.png`;
-  }
-}
